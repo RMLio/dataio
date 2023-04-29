3123bf0c18fc7a5d5c3fb90d1d9027e234a117ca
Jozef Jankaj <j.jankaj@ugent.be>
2023-04-29 12:59:30 +0200

    Rewrite DatabaseTest to use JUnit 5 bindings.
    Test cases are grouped into nested classes, as we only want to start a
    specific container for a specific test case

--- src/test/java/be/ugent/idlab/knows/dataio/access/DatabaseTest.java
+++ src/test/java/be/ugent/idlab/knows/dataio/access/DatabaseTest.java
@@ -1,38 +1,32 @@
 package be.ugent.idlab.knows.dataio.access;
 
-import be.ugent.idlab.knows.dataio.access.utils.ContainerManager;
 import be.ugent.idlab.knows.dataio.source.CSVSource;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
 import com.opencsv.enums.CSVReaderNullFieldIndicator;
 import com.opencsv.exceptions.CsvException;
-import org.junit.jupiter.api.AfterEach;
+import org.apache.ibatis.jdbc.ScriptRunner;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.testcontainers.containers.JdbcDatabaseContainer;
+import org.testcontainers.containers.*;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.shaded.org.apache.commons.io.input.BOMInputStream;
 
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@Testcontainers
 public class DatabaseTest {
 
     private static final String QUERY = "SELECT * FROM Patient";
-    private final ContainerManager manager = new ContainerManager();
-
-    @AfterEach
-    public void afterTest() {
-        JdbcDatabaseContainer<?> container = manager.getContainer();
-        if (container != null && container.isRunning()) {
-            container.stop();
-        }
-    }
 
     private void runTest(Access access, CSVSource expected) {
         try {
@@ -46,66 +40,24 @@ public class DatabaseTest {
         }
     }
 
-    @Test()
-    public void testPostgres() {
-        manager.startPostgresContainer();
-        manager.prepareDatabase("src/test/resources/db_setup/postgres_setup.sql");
-        Access access = getRDBAccess(DatabaseType.POSTGRES);
+    private void initializeDatabase(String scriptPath, JdbcDatabaseContainer<?> container) {
+        try (Connection conn = DriverManager.getConnection(container.getJdbcUrl(), container.getUsername(), container.getPassword())) {
+            ScriptRunner runner = new ScriptRunner(conn);
+            Reader reader = new BufferedReader(new FileReader(scriptPath));
+            runner.setLogWriter(null); // ScriptRunner will output the contents of the SQL file to System.out by default
 
-        String[] header = new String[]{"id", "firstname", "lastname", "sex", "weight", "height", "birthdate", "entrancedate", "paidinadvance", "photo"};
-        String[] expectedValues = new String[]{"10", "Monica", "Geller", "female", "80.25", "1.65", "1981-10-10", "2009-10-10 12:12:22", "f", "89504E470D0A1A0A0000000D49484452000000050000000508060000008D6F26E50000001C4944415408D763F9FFFEBFC37F062005C3201284D031F18258CD04000EF535CBD18E0E1F0000000049454E44AE426082"};
-        CSVSource expected = new CSVSource(header, expectedValues, access.getDataTypes());
-
-        runTest(access, expected);
+            runner.runScript(reader);
+        } catch (SQLException | FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    @Test
-    public void testMySQL() {
-        manager.startMySQLContainer();
-        manager.prepareDatabase("src/test/resources/db_setup/mysql_setup.sql");
-        Access access = getRDBAccess(DatabaseType.MYSQL);
-
-        String[] header = new String[]{"ID", "FirstName", "LastName", "Sex", "Weight", "Height", "BirthDate", "EntranceDate", "PaidInAdvance", "Photo"};
-        String[] expectedValues = new String[]{"10", "Monica", "Geller", "female", "80.25", "1.65", "1981-10-10", "2009-10-10 12:12:22", "0", "89504E470D0A1A0A0000000D49484452000000050000000508060000008D6F26E50000001C4944415408D763F9FFFEBFC37F062005C3201284D031F18258CD04000EF535CBD18E0E1F0000000049454E44AE426082"};
-        CSVSource expected = new CSVSource(header, expectedValues, access.getDataTypes());
-
-        runTest(access, expected);
-    }
-
-    @Test
-    public void testMSSQL() {
-        manager.startMSSQLContainer();
-        manager.prepareDatabase("src/test/resources/db_setup/mssql_setup.sql");
-        Access access = getRDBAccess(DatabaseType.SQL_SERVER);
-
-        String[] header = new String[]{"ID", "FirstName", "LastName", "Sex", "Weight", "Height", "BirthDate", "EntranceDate", "PaidInAdvance", "Photo"};
-        String[] expectedValues = new String[]{"10", "Monica", "Geller", "female", "80.25", "1.65", "1981-10-10", "2009-10-10 12:12:22.0", "0", "383935303445343730443041314130413030303030303044343934383434"};
-        CSVSource expected = new CSVSource(header, expectedValues, access.getDataTypes());
-
-        runTest(access, expected);
-    }
-
-    @Test
-    public void testOracle() {
-        manager.startOracleContainer();
-        manager.prepareDatabase("src/test/resources/db_setup/oracle_setup.sql");
-        Access access = getRDBAccess(DatabaseType.ORACLE);
-
-        CSVSource expected = new CSVSource(
-                new String[]{"ID", "NAME"},
-                new String[]{"10", "Venus"},
-                access.getDataTypes()
-        );
-
-        runTest(access, expected);
-    }
-
-    private Access getRDBAccess(DatabaseType type) {
+    private Access getRDBAccess(DatabaseType type, JdbcDatabaseContainer<?> container) {
         return new RDBAccess(
-                this.manager.getDbURL(),
+                container.getJdbcUrl(),
                 type,
-                this.manager.getContainer().getUsername(),
-                this.manager.getContainer().getPassword(),
+                container.getUsername(),
+                container.getPassword(),
                 QUERY,
                 "text/csv");
     }
@@ -126,6 +78,95 @@ public class DatabaseTest {
                     .collect(Collectors.toList());
         } catch (CsvException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    @Nested
+    class PostgreSQLTest {
+        @Container
+        private final PostgreSQLContainer<?> postgreSQLContainer = (PostgreSQLContainer<?>) new PostgreSQLContainer("postgres:latest")
+                .withUsername("postgres")
+                .withPassword("")
+                .withDatabaseName("test")
+                .withEnv("POSTGRES_HOST_AUTH_METHOD", "trust");
+
+        @Test
+        public void testPostgres() {
+            initializeDatabase("src/test/resources/db_setup/postgres_setup.sql", postgreSQLContainer);
+            Access access = getRDBAccess(DatabaseType.POSTGRES, postgreSQLContainer);
+
+            String[] header = new String[]{"id", "firstname", "lastname", "sex", "weight", "height", "birthdate", "entrancedate", "paidinadvance", "photo"};
+            String[] expectedValues = new String[]{"10", "Monica", "Geller", "female", "80.25", "1.65", "1981-10-10", "2009-10-10 12:12:22", "f", "89504E470D0A1A0A0000000D49484452000000050000000508060000008D6F26E50000001C4944415408D763F9FFFEBFC37F062005C3201284D031F18258CD04000EF535CBD18E0E1F0000000049454E44AE426082"};
+            CSVSource expected = new CSVSource(header, expectedValues, access.getDataTypes());
+
+            runTest(access, expected);
+        }
+    }
+
+    @Nested
+    class MSSQLTest {
+        @Container
+        private final MSSQLServerContainer<?> mssqlServerContainer = new MSSQLServerContainer<>("mcr.microsoft.com/mssql/server:latest")
+                .acceptLicense()
+                .withPassword("YourSTRONG!Passw0rd;");
+
+        @Test
+        public void testMSSQL() {
+            initializeDatabase("src/test/resources/db_setup/mssql_setup.sql", mssqlServerContainer);
+            Access access = getRDBAccess(DatabaseType.SQL_SERVER, mssqlServerContainer);
+
+            String[] header = new String[]{"ID", "FirstName", "LastName", "Sex", "Weight", "Height", "BirthDate", "EntranceDate", "PaidInAdvance", "Photo"};
+            String[] expectedValues = new String[]{"10", "Monica", "Geller", "female", "80.25", "1.65", "1981-10-10", "2009-10-10 12:12:22.0", "0", "383935303445343730443041314130413030303030303044343934383434"};
+            CSVSource expected = new CSVSource(header, expectedValues, access.getDataTypes());
+
+            runTest(access, expected);
+        }
+    }
+
+    @Nested
+    class MySQLTestcase {
+        @Container
+        private final MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:latest")
+                .withUsername("root")
+                .withPassword("")
+                .withConfigurationOverride("db_setup/mysql_override")
+                .withEnv("allowPublicKeyRetrieval", "true")
+                .withEnv("useSSL", "false");
+
+        @Test
+        public void testMySQL() {
+            initializeDatabase("src/test/resources/db_setup/mysql_setup.sql", mySQLContainer);
+            Access access = getRDBAccess(DatabaseType.MYSQL, mySQLContainer);
+
+            String[] header = new String[]{"ID", "FirstName", "LastName", "Sex", "Weight", "Height", "BirthDate", "EntranceDate", "PaidInAdvance", "Photo"};
+            String[] expectedValues = new String[]{"10", "Monica", "Geller", "female", "80.25", "1.65", "1981-10-10", "2009-10-10 12:12:22", "0", "89504E470D0A1A0A0000000D49484452000000050000000508060000008D6F26E50000001C4944415408D763F9FFFEBFC37F062005C3201284D031F18258CD04000EF535CBD18E0E1F0000000049454E44AE426082"};
+            CSVSource expected = new CSVSource(header, expectedValues, access.getDataTypes());
+
+            runTest(access, expected);
+        }
+    }
+
+    @Nested
+    class OracleTestcase {
+        @Container
+        private final OracleContainer oracleContainer = new OracleContainer("gvenzl/oracle-xe:latest")
+                .withUsername("rmlmapper_test")
+                .withPassword("test")
+                .withEnv("NLS_LANG", "American_America.WE8ISO8859P1")
+                .waitingFor(Wait.forLogMessage(".*DATABASE IS READY TO USE!.*", 1));
+
+        @Test
+        public void testOracle() {
+            initializeDatabase("src/test/resources/db_setup/oracle_setup.sql", oracleContainer);
+            Access access = getRDBAccess(DatabaseType.ORACLE, oracleContainer);
+
+            CSVSource expected = new CSVSource(
+                    new String[]{"ID", "NAME"},
+                    new String[]{"10", "Venus"},
+                    access.getDataTypes()
+            );
+
+            runTest(access, expected);
         }
     }
 }

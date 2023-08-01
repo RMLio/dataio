83eba1b6753824580b6ef0f796febd39151f2d8c
Gerald Haesendonck <gerald.haesendonck@ugent.be>
2023-08-01 13:30:25 +0200

    Make CSVSourceIterator a subclass of CSVWSourceIterator

--- src/main/java/be/ugent/idlab/knows/dataio/iterators/CSVSourceIterator.java
+++ src/main/java/be/ugent/idlab/knows/dataio/iterators/CSVSourceIterator.java
@@ -2,33 +2,18 @@ package be.ugent.idlab.knows.dataio.iterators;
 
 import be.ugent.idlab.knows.dataio.access.Access;
 import be.ugent.idlab.knows.dataio.iterators.csvw.CSVWConfiguration;
-import be.ugent.idlab.knows.dataio.source.Source;
 
 import java.io.IOException;
 import java.sql.SQLException;
 
 /**
  * An iterator for CSV Sources.
- * Implementation relies on CSVWSourceIterator, as regular CSV is just a special case of CSVW.
+ * Subclass CSVWSourceIterator, as regular CSV is just a special case of CSVW.
  */
-public class CSVSourceIterator extends SourceIterator {
+public class CSVSourceIterator extends CSVWSourceIterator {
     private static final long serialVersionUID = -352163251763906563L;
-    private final CSVWSourceIterator iterator;
 
     public CSVSourceIterator(Access access) throws SQLException, IOException {
-        this.iterator = new CSVWSourceIterator(access, CSVWConfiguration.DEFAULT);
-    }
-
-    public Source next() {
-        return this.iterator.next();
-    }
-
-    public boolean hasNext() {
-        return this.iterator.hasNext();
-    }
-
-    @Override
-    public void close() throws IOException {
-        this.iterator.close();
+        super(access, CSVWConfiguration.DEFAULT);
     }
 }

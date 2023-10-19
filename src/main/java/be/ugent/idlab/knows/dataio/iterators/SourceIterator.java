b7b18002070da88ed6000e247e4da7304b970322
Gerald Haesendonck <gerald.haesendonck@ugent.be>
2023-10-19 11:34:36 +0200

    SourceIterator: review

--- src/main/java/be/ugent/idlab/knows/dataio/iterators/SourceIterator.java
+++ src/main/java/be/ugent/idlab/knows/dataio/iterators/SourceIterator.java
@@ -2,6 +2,7 @@ package be.ugent.idlab.knows.dataio.iterators;
 
 import be.ugent.idlab.knows.dataio.record.Record;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.Iterator;
 import java.util.function.Consumer;
@@ -10,6 +11,7 @@ import java.util.function.Consumer;
  * Iterator over a source. Generates Records from an Access object.
  */
 public abstract class SourceIterator implements Iterator<Record>, Serializable, AutoCloseable {
+    @Serial
     private static final long serialVersionUID = 7064007069397357197L;
 
     @Override

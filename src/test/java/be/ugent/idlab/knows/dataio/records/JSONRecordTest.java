4b04dcda649a8b27311a62f25ad640670c3b31c7
Aron Buzogany <aronsaps@gmail.com>
2023-11-29 09:36:32 +0100

    replaced variable datatype with interface it implements

--- src/test/java/be/ugent/idlab/knows/dataio/records/JSONRecordTest.java
+++ src/test/java/be/ugent/idlab/knows/dataio/records/JSONRecordTest.java
@@ -2,10 +2,12 @@ package be.ugent.idlab.knows.dataio.records;
 
 import be.ugent.idlab.knows.dataio.access.LocalFileAccess;
 import be.ugent.idlab.knows.dataio.iterators.JSONSourceIterator;
+import be.ugent.idlab.knows.dataio.iterators.SourceIterator;
 import be.ugent.idlab.knows.dataio.utils.NAMESPACES;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -13,8 +15,8 @@ public class JSONRecordTest {
 
     @Test
     public void testTwoDotsIterator() throws Exception{
-        JSONSourceIterator it = new JSONSourceIterator(new LocalFileAccess("artworks.json", "src/test/resources/json/jsonPathTwoDotsTest", NAMESPACES.XSD + "string"), "$");
-        ArrayList<String> results = new ArrayList<>();
+        SourceIterator it = new JSONSourceIterator(new LocalFileAccess("artworks.json", "src/test/resources/json/jsonPathTwoDotsTest", NAMESPACES.XSD + "string"), "$");
+        List<String> results = new ArrayList<>();
         it.forEachRemaining(r -> {
             results.add(r.get("..children[*].id").toString());
         });

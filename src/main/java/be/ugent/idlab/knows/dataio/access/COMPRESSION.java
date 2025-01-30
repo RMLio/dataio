20a91b9411a839e56987d38978ab2d406c5e7d3c
Jozef Jankaj <jozef.jankaj@ugent.be>
2025-01-30 11:59:49 +0100

    chore: return COMPRESSION.java
    This file is utilized by RMLMapper, we do not want to break it.

---
+++ src/main/java/be/ugent/idlab/knows/dataio/access/COMPRESSION.java
@@ -0,0 +1,10 @@
+package be.ugent.idlab.knows.dataio.access;
+
+/**
+ * Class describing compression methods.
+ *
+ * Utilized by RMLMapper, do not remove!
+ */
+public class COMPRESSION {
+    public static final String GZIP = "gzip";
+}
